b6c8134e665be5591d0dd071f90f545c17adf710
Michael Martorella <michaelmartorella@gmail.com>
2021-04-08 15:45:38 -0400

    Add runner functionality

--- remy/runner.py
+++ remy/runner.py
@@ -7,7 +7,18 @@ from remy import telegram_helper
 
 def main():
     """Runs our bot."""
-    pass
+    updater = telegram_helper.UPDATER
+
+    logging.info("Starting the bot!")
+
+    try:
+        updater.start_polling()
+        updater.idle()
+    except telegram.error.TelegramError as e:
+        logging.error(f"Something went wrong! {e}")
+        raise e
+
+    logging.info("Bot shutting down. See ya next time!")
 
 if __name__ == "__main__":
     main()
