655248e4ad7d7bc5fd04f33b716aacb5f48d2b72
Marto32 <michaelmartorella@gmail.com>
2021-04-09 09:22:08 -0400

    Add custom exceptions and better error handling behavior

--- remy/telegram_helper.py
+++ remy/telegram_helper.py
@@ -9,6 +9,7 @@ from telegram.ext import (
 )
 
 from remy import config
+from remy import exceptions
 from remy import spoonacular_helper as sp
 
 
@@ -66,29 +67,15 @@ def recipes_for_ingredients(update, context):
     """Returns html formatted recipes given the input string."""
     ingredients = ''.join(context.args).lower()
     if not ingredients:
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text="I need ingredients to search\!",
-            parse_mode=telegram.ParseMode.MARKDOWN_V2
-        )
-        return
+        logging.info("No ingredients provided!")
+        raise exceptions.MissingIngredientError()
 
     recipe_ids = spoon.get_recipe_ids_for_ingredients(ingredients)
-
     if not recipe_ids:
         logging.info("No recipes found.")
-        message = (
-            "Yikes! I couldn't find anything for those ingredients. "
-            "Sorry about that. Please try some different ones."
-        )
-        context.bot.send_message(
-            chat_id=update.effective_chat.id,
-            text=message
-        )
-        return
+        raise exceptions.RecipesNotFoundError()
 
     recipes = spoon.get_recipes_for_ids(recipe_ids)
-
     for recipe in recipes:
         message, parse_mode = format_message_and_get_parse_mode(recipe) 
         logging.info("Sending...")
@@ -106,20 +93,7 @@ def random_recipe(update, context):
     all_args = ",".join(context.args).lower().split(",")
     for value in all_args:
         if value not in config.ALLOWED_TAGS:
-            message = (
-                "Only the following are allowed as tags: "
-                "[Diets](https://spoonacular.com/food-api/docs#Diets), "
-                "[Intolerances](https://spoonacular.com/food-api/docs#Intolerances), "
-                "[Cuisines](https://spoonacular.com/food-api/docs#Cuisines), "
-                "and [Meal Types](https://spoonacular.com/food-api/docs#Meal-Types)\."
-                " Please try your search again with a valid tag\."
-            )
-            context.bot.send_message(
-                chat_id=update.effective_chat.id,
-                text=message,
-                parse_mode=telegram.ParseMode.MARKDOWN_V2
-            )
-            return
+            raise exceptions.InvalidRandomTagError()
 
     # Consolidate valid tags into single query
     tags = ",".join(all_args)
@@ -183,18 +157,42 @@ def error_handler(update, context):
     """Handles errors we get while executing commands."""
     logging.error(
         msg="Something went wrong when trying to handle an update.",
-        exc_info=context.error)
-    
-    # Handle quota errors
-    if isinstance(context.error, sp.QuotaError):
+        exc_info=context.error
+    )
+
+    parse_mode = None
+
+    # Handle quota error
+    if isinstance(context.error, exceptions.QuotaError):
         message = "We've hit our recipe quota for today! Come back tomorrow."
+    # Missing ingredient argument error
+    elif isinstance(context.error, exceptions.MissingIngredientError):
+        message = "I need ingredients to search!"
+    # No recipes found error
+    elif isinstance(context.error, exceptions.RecipesNotFoundError):
+        message = (
+            "Yikes! I couldn't find anything for those ingredients. "
+            "Sorry about that. Please try some different ones."
+        )
+    # Invalid random tag error
+    elif isinstance(context.error, exceptions.InvalidRandomTagError):
+        message = (
+            "Only the following are allowed as tags: "
+            "[Diets](https://spoonacular.com/food-api/docs#Diets), "
+            "[Intolerances](https://spoonacular.com/food-api/docs#Intolerances), "
+            "[Cuisines](https://spoonacular.com/food-api/docs#Cuisines), "
+            "and [Meal Types](https://spoonacular.com/food-api/docs#Meal-Types)\."
+            " Please try your search again with a valid tag\."
+        )
+        parse_mode = telegram.ParseMode.MARKDOWN_V2
     # Catchall for other errors
     else:
         message = "Something went wrong with that last one! Try again or use /help"
 
     context.bot.send_message(
         chat_id=update.effective_chat.id,
-        text=message
+        text=message,
+        parse_mode=parse_mode
     )
 
 # Handler registration
@@ -217,9 +215,11 @@ handlers = [
     UNKNOWN_HANDLER
 ]
 
+logging.info("Registering handlers with dispatcher...")
+
 for handler in handlers:
-    logging.info(f"Adding the handler: {handler}")
     DISPATCHER.add_handler(handler)
 
-logging.info(f"Adding our error handler.")
 DISPATCHER.add_error_handler(error_handler)
+
+logging.info("Handlers added to dispatcher.")
